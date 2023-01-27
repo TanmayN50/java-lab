2bbd56d19cb076b5cfe94f1d20500caf5bec4eca
TanmayN50 <122600298+TanmayN50@users.noreply.github.com>
2023-01-27 09:34:11 +0530

    Add files via upload

---
+++ SimpleApplet.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;;
+public class SimpleApplet extends Applet{
+
+	public void paint(Graphics g) {
+		g.drawString("Wellcome to Applet",100,150);
+	}
+		
+}
