51df9bbd1658051c88c6c434809a16181a73382c
TanmayN50 <122600298+TanmayN50@users.noreply.github.com>
2023-01-27 09:41:53 +0530

    Add files via upload

---
+++ Compute.java
@@ -0,0 +1,10 @@
+import Converter.Dollar;
+public class Compute{
+public static void main(String s[]){
+	Dollar d=new Dollar();
+	d.currency_convert();
+	d.Distance_convert();
+	d.Time_convert();
+}
+}
+
